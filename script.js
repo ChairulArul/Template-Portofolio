ce4187d12fcbe398c474cc8bd1d19a95bdf35b29
chairul <chairul@students.mikroskil.ac.id>
2024-12-18 00:18:40 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    if (name && email && message) {
+      alert("Pesan berhasil dikirim!");
+      document.getElementById("contact-form").reset();
+    } else {
+      alert("Mohon lengkapi semua field!");
+    }
+  });
